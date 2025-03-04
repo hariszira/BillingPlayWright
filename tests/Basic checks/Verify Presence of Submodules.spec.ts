e1895f4b1d6b4a8524305841302c6e8d0a334f17
hariszira <haris.smajic@ziragroup.com>
2025-03-04 09:15:59 +0100

    ok

--- tests/Basic checks/Verify Presence of Submodules.spec.ts
+++ tests/Basic checks/Verify Presence of Submodules.spec.ts
@@ -1,33 +1,30 @@
 import { test } from '../../fixtures/fixtures';
 import { expect } from '@playwright/test';
 
-test.describe("Login to Billing Module and Verify Presence od Submodules", () => {
+test.describe("Login to Billing Module and Verify Presence of Submodules", () => {
   test.beforeEach(async ({ loginPage }) => {
     await loginPage.login();
   });
+
   test('Verify Presence of Submodules', async ({ page }) => {
   
-    // Now that we're logged in, we can proceed with the test actions
     const BillingHeader = page.locator('div.name:has-text("BILLING")');
     await expect(BillingHeader).toBeVisible({ timeout: 5000 });  // Assert that the Billing module is visible
   
-    // Check the presence of submodules
-    const sections = [
-      "Charge Management", 
-      "Billing Profile", 
-      "Approval", 
-      "IBS Processing", 
-      "Invoicing", 
-      "Finance", 
-      "Administration", 
-      "Penalty"
-    ];
+    // Check the presence of submodules on Home Page
+    const sections = ["Charge Management","Billing Profile","Approval","IBS Processing","Invoicing","Finance","Administration","Penalty"];
     
     for (const section of sections) {
       const locator = page.locator(`h2.label:has-text("${section}")`);
       await expect(locator).toBeVisible();
     }
 
+    // Check the presence of submodules in Menu
+    await page.locator('.fal.fa-bars').click();
+
+    for (const item of sections) {
+      const menuItemLocator = page.getByRole('link', { name: item })
+      await expect(menuItemLocator).toBeVisible();
+    }
   });
 });
-
